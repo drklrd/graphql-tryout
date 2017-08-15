576ccbe36d683cb5f9cc9dc12d0c63ba59c3843a
drklrd <srvbhattarai@gmail.com>
2017-08-15 21:41:12 +0545

    including mongodb query as well

--- schema/types/me.js
+++ schema/types/me.js
@@ -3,13 +3,16 @@ const {
 	GraphQLID,
 	GraphQLString,
 	GraphQLNonNull,
-	GraphQLList
+	GraphQLList,
+	GraphQLInt
 } = require('graphql');
 
 
 const { fromSnakeCase } = require('../../lib/util');
 const ContestType = require('./contest');
 const pgdb = require('../../database/pgdb');
+const mdb = require('../../database/mdb');
+
 
 module.exports = new GraphQLObjectType({
 	name : 'MeType',
@@ -34,6 +37,24 @@ module.exports = new GraphQLObjectType({
 			type : new GraphQLList(ContestType),
 			resolve(obj,args,{ pgPool }){
 				return pgdb(pgPool).getContests(obj);
+			}
+		},
+		contestsCount : {
+			type : GraphQLInt,
+			resolve(obj,args, { mPool }, { fieldName }){
+				return mdb(mPool).getCounts(obj,fieldName);
+			}
+		},
+		namesCount : {
+			type : GraphQLInt,
+			resolve(obj,args, { mPool }, { fieldName }){
+				return mdb(mPool).getCounts(obj,fieldName);
+			}
+		},
+		votesCount : {
+			type : GraphQLInt,
+			resolve(obj,args, { mPool }, { fieldName }){
+				return mdb(mPool).getCounts(obj,fieldName);
 			}
 		}
 
